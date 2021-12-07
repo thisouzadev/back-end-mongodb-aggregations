658021862972428030e98750a389051bc7a3ed07
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-12-07 11:29:53 -0300

    desafio 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -7,28 +7,32 @@ const actor = [
 ];
 db.movies.aggregate(
   [
-    {$match: {
-      countries: "USA",
-      "tomatoes.viewer.rating": {$gte: 3 },
-      cast: { $exists: 1 },
+    {
+      $match: {
+        countries: "USA",
+        "tomatoes.viewer.rating": { $gte: 3 },
+        cast: { $exists: 1 },
+      },
     },
-  },
-    { $addFields: {
-      num_favs: { $size: { $setIntersection: ["$cast", actor] } },
+    {
+      $addFields: {
+        num_favs: { $size: { $setIntersection: ["$cast", actor] } },
+      },
     },
-  },
-    { $sort: {
+    {
+      $sort: {
         num_favs: -1,
         "tomatoes.viewer.rating": -1,
         title: -1,
       },
     },
-    { $project: {
-      _id: 0,
-      title: 1,
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+      },
     },
-  }, 
-  { $skip: 24 },
-  { $limit: 1 },
+    { $skip: 24 },
+    { $limit: 1 },
   ],
 );
