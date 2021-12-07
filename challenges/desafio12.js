4335c163d88937cc6a7bedf418d17d2e808084b9
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2021-12-07 19:08:35 -0300

    desafio 12 e 13

---
+++ challenges/desafio12.js
@@ -0,0 +1,30 @@
+db.trips.aggregate(
+  [
+    {
+    $project: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+      startStationName: 1,
+    },
+  },
+  {
+    $match: {
+      diaDaSemana: 5,
+    },
+  },
+    {
+      $group: {
+        _id: "$startStationName",
+        total: { $sum: 1 },
+      },
+    },
+    { $sort: { total: -1 } },
+    { $limit: 1 },
+    {
+      $project: {
+        nomeEstacao: "$_id",
+        total: "$total",
+        _id: 0,
+      },
+    },
+  ],
+);
